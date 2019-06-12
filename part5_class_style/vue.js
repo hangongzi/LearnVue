fc5267fb7c70ca08b557a1e3724667bab0a64752
hangongzi <hanjieyouxiang2615@163.com>
2019-06-12 17:21:32 +0800

    add part5

---
+++ part5_class_style/vue.js
@@ -0,0 +1,13 @@
+var vm = new Vue({
+    el:'#demo',
+    data:{
+        class_test:{
+            isActive: true,
+            hasError: false
+        },
+        style_test:{
+            color:'red',
+            fontSize:'40px'
+        }
+    }
+})
